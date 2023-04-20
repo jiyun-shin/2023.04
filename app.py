607ea6886ea3aad6949bc8ec0e92c99e26edc032
EC2 Default User <ec2-user@ip-172-31-10-189.ap-northeast-2.compute.internal>
2023-04-20 02:31:28 +0000

    commit-0420-1131

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask (__name__)
 
 @app.route('/')
 def hello_world():
-    return 'plz press the like btn !! by wookim'
+    return '2023-04-20_11:30'
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
